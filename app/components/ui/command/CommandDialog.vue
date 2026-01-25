14cdce7980eb0b49547c2441bac6f75dd93ad686
Georgi Georgiev <gatanasovgeorgiev@gmail.com>
2026-01-25 04:05:56 +0200

    fix: hide close button in command palette dialog

--- app/components/ui/command/CommandDialog.vue
+++ app/components/ui/command/CommandDialog.vue
@@ -18,7 +18,7 @@ const forwarded = useForwardPropsEmits(props, emits)
 
 <template>
   <Dialog v-slot="slotProps" v-bind="forwarded">
-    <DialogContent class="overflow-hidden p-0 ">
+    <DialogContent class="overflow-hidden p-0" :show-close-button="false">
       <DialogHeader class="sr-only">
         <DialogTitle>{{ title }}</DialogTitle>
         <DialogDescription>{{ description }}</DialogDescription>
